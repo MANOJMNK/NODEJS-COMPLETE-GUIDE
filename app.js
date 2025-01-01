72a67956440af5b0d26d24d6916f131928661c57
Manoj Kumar <mkumar16@deloitte.com>
2025-01-01 19:05:07 +0530

    added routes for expressjs

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRouter);
+app.use(shopRouter);
+
 app.use((req, res, next) => {
-    console.log("In the Middleware");
-    next();
+    res.status(404).send("<h1>Page not found</h1>");
 });
-app.use((req, res, next) => {
-    console.log("In the another Middleware");
-    res.send('<h1>Welcone to Express JS</h1>')
-})
+
 app.listen(3000);
