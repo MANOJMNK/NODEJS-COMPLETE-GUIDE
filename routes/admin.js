85464586e4ab36c19de91fa37d27cd5bab837c1e
Manoj Kumar <mkumar16@deloitte.com>
2025-01-02 22:40:24 +0530

    Implemented static template and css rendered on router

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,15 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootdir = require('../util/path');
 
 const router = express.Router();
 
 router.use('/add-products', (req, res, next) => {
     console.log("In the Middleware add product");
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button>Add Product</button></form>');
+    //res.send('<form action="/product" method="POST"><input type="text" name="title"><button>Add Product</button></form>');
+    res.sendFile(path.join(rootdir, 'views', 'add-product.html'));
 });
 router.use('/product', (req, res, next) => {
     console.log(res.body);
